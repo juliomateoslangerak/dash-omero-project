366377e5d61de41317c4cee4075ea8322b9f892e
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-03-08 15:42:38 +0100

    Moved things around renamed to omero-metrics

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="omero-dash-webapp",
+    name="omero_metrics",
     version="0.1.0",
     description="OMERO example minimal web app",
     packages=find_packages(),
